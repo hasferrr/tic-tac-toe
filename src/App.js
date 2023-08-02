e31bb55ce0f418793a693744f33e94cfdb3b8483
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-08-02 22:01:48 +0700

    implements assign tic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill('X'))
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState('X')
+
+  const Gameboard = (() => {
+
+    const assign = event => {
+      if (board[event.target.id] === null) {
+        event.target.innerText = turn
+        setBoard((() => {
+          const copy = [...board]
+          copy[event.target.id] = turn
+          return copy
+        })())
+        setTurn(turn === 'X' ? 'O' : 'X')
+      }
+    }
+
+    const resetBoard = () => {
+      setBoard(Array(9).fill(null))
+      setTurn('X')
+    }
+
+    return { assign, resetBoard }
+  })()
 
   return (
     <div className='Board'>
       {board.map((box, index) =>
-        <div key={index} className='box'>
+        <div key={index} className='box' id={index} onClick={Gameboard.assign} >
           {box}
         </div>
       )}
